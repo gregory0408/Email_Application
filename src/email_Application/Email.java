c013da611e6e2c52ce037b995b1a6e40db590205
utilisateur1 <ngonganggreg@yahoo.com>
2024-05-13 17:04:33 -0400

    Creation of an email aplication

---
+++ src/email_Application/Email.java
@@ -0,0 +1,29 @@
+package email_Application;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	
+	//constructor to receive the first name and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATED: " + this.firstName +" " + this.lastName);
+	}
+	
+	//Ask for department
+	
+	//Generate random password
+	
+	// Set the mailbox capacity
+	
+	// Set the alternate email
+	
+	// Change the password
+
+}
---
+++ src/email_Application/Emailapp.java
@@ -0,0 +1,10 @@
+package email_Application;
+
+public class Emailapp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("John", "smith");
+
+	}
+
+}
